2ce9620363f932af24fdd79b6385c9ef9fd793e5
hammadlodhi <hammadak05@gmail.com>
2025-08-12 17:32:41 +0500

    backend url configured

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,1 +1,10 @@
-export const BACKEND_URL = "http://localhost:5000";
+export const development = "http://localhost:5000";
+export const production = "https://api.directwholesalecars.com.au";
+
+// Assign a value to `curr_env`, not a type
+const curr_env: "Prod" | "DEV" = "DEV";
+
+//   //@ts-ignore
+curr_env === "DEV" ? development : production;
+export const BACKEND_URL = "https://api.directwholesalecars.com.au";
+// export const BACKEND_URL = "http://localhost:5000";
