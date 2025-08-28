d799b386afa4937dbf372f92d92c2687325ff2d8
areebakhan3 <arkhan.2003k@gmail.com>
2025-08-28 02:27:01 -0700

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,7 +77,7 @@ const networkSteps = [
     subtitle: "Submit your dealer application with business documentation",
   },
   {
-    image: <img src="/images/verification.png" alt="Verification" color="black"/>,
+    image: <img src="/images/verification.png" alt="Verification" />,
     title: "Verification",
     subtitle: "Our team verifies your credentials and business license",
   },
